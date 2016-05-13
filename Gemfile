44caf722e58648d01cd229e395b622bde3a085c7
Alex Hemard <alexhemard@gmail.com>
2016-05-13 02:10:33 -0500

    Rails 5 bump

--- Gemfile
+++ Gemfile
@@ -1,18 +1,18 @@
 ruby "2.3.0"
 source "https://rubygems.org"
 
-gem "rails",        "4.1.1"
-gem "sass-rails",   "~> 4.0.3"
-gem "uglifier",     ">= 1.3.0"
-gem "jquery-rails"
-gem "coffee-rails", "~> 4.0.0"
-gem "therubyracer", platforms: :ruby
-gem "jbuilder",     "~> 2.0"
-gem "unicorn"
-gem "paperclip"
 gem "aws-sdk"
+gem "coffee-rails",       "~> 4.1.1"
+gem "font-awesome-sass",  "~> 4.6.2"
+gem "jbuilder",           "~> 2.0"
+gem "jquery-rails",       "~> 4.1.1"
+gem "paperclip"
 gem "pg"
-gem "font-awesome-sass"
+gem "rails",              "5.0.0rc1"
+gem "sass-rails",         "~> 5.0.4"
+gem "therubyracer", platforms: :ruby
+gem "uglifier",           ">= 1.3.0"
+gem "unicorn"
 
 group :doc do
   gem "sdoc", "~> 0.4.0"
--- db/schema.rb
+++ db/schema.rb
@@ -16,17 +16,17 @@ ActiveRecord::Schema.define(version: 20140608011253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "images", force: true do |t|
+  create_table "images", force: :cascade do |t|
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "vote_arrogance",       limit: 8, default: 0
-    t.integer  "vote_swag",            limit: 8, default: 0
-    t.integer  "vote_swag_count",      limit: 8, default: 0
-    t.integer  "vote_arrogance_count", limit: 8, default: 0
+    t.bigint   "vote_arrogance",       default: 0
+    t.bigint   "vote_swag",            default: 0
+    t.bigint   "vote_swag_count",      default: 0
+    t.bigint   "vote_arrogance_count", default: 0
   end
 
 end
